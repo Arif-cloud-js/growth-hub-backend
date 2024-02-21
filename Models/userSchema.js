81b17af988a85dd7ca2b0db39c19bded86d763a3
Arif-cloud-js <m.arifalam450@gmail.com>
2024-02-21 04:57:25 -0800

    rmvd

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose= require('mongoose');
 const jwt      = require('jsonwebtoken');
-const bcrypt   =require('bcrypt');
 const { string } = require('@tensorflow/tfjs');
 const userSchema = new mongoose.Schema({
     username:
